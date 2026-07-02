a77a460a07729a1f371431dd7cb28fcba8fd18de
maaitlunghau <chunhau.py@gmail.com>
2026-07-02 22:55:41 +0700

    feat(practice02): add AbstractFacade generic crud base class

---
+++ Practice_02/Practice_02-ejb/src/main/java/com/fpt/sb/AbstractFacade.java
@@ -0,0 +1,55 @@
+package com.fpt.sb;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import java.util.List;
+
+public abstract class AbstractFacade<T> {
+
+    private final Class<T> entityClass;
+
+    public AbstractFacade(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
+
+    protected abstract EntityManager getEntityManager();
+
+    public void create(T entity) {
+        getEntityManager().persist(entity);
+    }
+
+    public void edit(T entity) {
+        getEntityManager().merge(entity);
+    }
+
+    public void remove(T entity) {
+        getEntityManager().remove(getEntityManager().merge(entity));
+    }
+
+    public T find(Object id) {
+        return getEntityManager().find(entityClass, id);
+    }
+
+    public List<T> findAll() {
+        CriteriaQuery<T> cq = getEntityManager().getCriteriaBuilder().createQuery(entityClass);
+        cq.select(cq.from(entityClass));
+        return getEntityManager().createQuery(cq).getResultList();
+    }
+
+    public List<T> findRange(int[] range) {
+        CriteriaQuery<T> cq = getEntityManager().getCriteriaBuilder().createQuery(entityClass);
+        cq.select(cq.from(entityClass));
+        return getEntityManager().createQuery(cq)
+                .setFirstResult(range[0])
+                .setMaxResults(range[1] - range[0] + 1)
+                .getResultList();
+    }
+
+    public int count() {
+        CriteriaQuery<Long> cq = getEntityManager().getCriteriaBuilder().createQuery(Long.class);
+        Root<T> rt = cq.from(entityClass);
+        cq.select(getEntityManager().getCriteriaBuilder().count(rt));
+        return getEntityManager().createQuery(cq).getSingleResult().intValue();
+    }
+}
