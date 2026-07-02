7fa341fb1268a88ddd05c062213b31b2e0d9c0fa
maaitlunghau <chunhau.py@gmail.com>
2026-07-02 22:54:54 +0700

    chore(practice02): add database script for EAD_Practice02

---
+++ Practice_02/scriptDB.sql
@@ -0,0 +1,48 @@
+-- EAD Practice_02
+-- Database script for Microsoft SQL Server
+
+USE master;
+GO
+
+IF DB_ID(N'EAD_Practice02') IS NULL
+BEGIN
+    CREATE DATABASE EAD_Practice02;
+END;
+GO
+
+USE EAD_Practice02;
+GO
+
+IF OBJECT_ID(N'dbo.accounts', N'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.accounts;
+END;
+GO
+
+CREATE TABLE dbo.accounts
+(
+    AccId      INT IDENTITY(1,1) NOT NULL,
+    Username   VARCHAR(10)  NOT NULL,
+    [Password] VARCHAR(20)  NOT NULL,
+    [Role]     VARCHAR(10)  NOT NULL CONSTRAINT DF_accounts_Role DEFAULT ('admin'),
+    [Image]    VARCHAR(200) NOT NULL,
+
+    CONSTRAINT PK_accounts PRIMARY KEY (AccId),
+    CONSTRAINT UQ_accounts_Username UNIQUE (Username),
+    CONSTRAINT CK_accounts_Role CHECK ([Role] IN ('admin', 'user'))
+);
+GO
+
+INSERT INTO dbo.accounts (Username, [Password], [Role], [Image])
+VALUES
+    ('admin',  '123456', 'admin', 'admin.jpg'),
+    ('john',   '123456', 'user',  'john.jpg'),
+    ('mary',   '123456', 'user',  'mary.jpg'),
+    ('peter',  '123456', 'user',  'peter.jpg'),
+    ('sarah',  '123456', 'user',  'sarah.jpg');
+GO
+
+SELECT AccId, Username, [Password], [Role], [Image]
+FROM dbo.accounts
+ORDER BY AccId;
+GO
